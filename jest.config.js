3d23b96aec5aa60ba803b1b4d0f0976a04c1fd3e
Tad Tarrant <69368213+crowbarmaster@users.noreply.github.com>
2023-09-09 09:25:13 -0400

    Update jest.config.js
    Remove old project refs.

--- jest.config.js
+++ jest.config.js
@@ -10,23 +10,10 @@ module.exports = {
   collectCoverage: true,
   projects: [
     {
-      name: 'keybase-notifications',
-      displayName: 'keybase-notifications',
-      testRegex: 'packages/keybase-notifications/__tests__',
-      testPathIgnorePatterns: ['/__tests__/payloads', '/__tests__/utils'],
-      setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    },
-    {
       name: 'automatic-releases',
       displayName: 'automatic-releases',
       testRegex: 'packages/automatic-releases/__tests__',
       testPathIgnorePatterns: ['/__tests__/payloads', '/__tests__/utils/', '/__tests__/assets'],
-      setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
-    },
-    {
-      name: 'aws-ssm-secrets',
-      displayName: 'aws-ssm-secrets',
-      testRegex: 'packages/aws-ssm-secrets/__tests__',
       setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
     },
   ],
@@ -37,15 +24,10 @@ module.exports = {
     },
   },
   collectCoverageFrom: [
-    '**/packages/keybase-notifications/**/*.ts',
     '**/packages/automatic-releases/**/*.ts',
-    '**/packages/aws-ssm-secrets/**/*.ts',
     '!**/__tests__/**',
     '!**/dist/**',
-    '!**/packages/keybase-notifications/src/index.ts',
-    '!**/packages/keybase-notifications/src/utils.ts',
     '!**/packages/automatic-releases/src/index.ts',
     '!**/packages/automatic-releases/src/uploadReleaseArtifacts.ts',
-    '!**/packages/aws-ssm-secrets/src/index.ts',
   ],
 };

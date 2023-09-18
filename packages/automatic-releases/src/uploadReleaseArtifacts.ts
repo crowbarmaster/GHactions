acad4cc9bd8e8511a7077c90d09c53e8c1373618
Tad Tarrant <69368213+crowbarmaster@users.noreply.github.com>
2023-09-17 21:28:41 -0400

    Attempted fix for corrupt uploads.

--- packages/automatic-releases/src/uploadReleaseArtifacts.ts
+++ packages/automatic-releases/src/uploadReleaseArtifacts.ts
@@ -1,6 +1,6 @@
 import * as core from '@actions/core';
 import {globby} from 'globby';
-import {createReadStream} from 'fs';
+import {createReadStream, statSync} from 'fs';
 import * as types from '@octokit/types';
 import path from 'path';
 import md5File from 'md5-file';
@@ -24,9 +24,25 @@ export const uploadReleaseArtifacts = async (
 
     for (const filePath of paths) {
       core.info(`Uploading: ${filePath}`);
+      const stat = statSync(filePath);
+      if (!stat.isFile()) {
+        core.debug(`Skipping ${filePath}, since its not a file`);
+        return;
+      }
+      const file_size = stat.size;
+      if (file_size === 0) {
+        core.debug(`Skipping ${filePath}, since its size is 0`);
+        return;
+      }
+
       const nameWithExt = path.basename(filePath);
       try {
         await client.rest.repos.uploadReleaseAsset({
+          url: releaseParams.data.upload_url,
+          headers: {
+            'content-type': 'binary/octet-stream',
+            'content-length': file_size,
+          },
           owner: releaseParams.owner,
           repo: releaseParams.repo,
           release_id: uploadId,
